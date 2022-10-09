2eb4cc7c05b3a5c16a3fd2795d49f809a35b3a1b
Mario Scalas <mario.scalas@gmail.com>
2022-10-09 19:48:28 +0100

    Added comments.

--- src/zoorl/zoorl/adapters/create_url_hash_handler.py
+++ src/zoorl/zoorl/adapters/create_url_hash_handler.py
@@ -1,3 +1,5 @@
+"""AWS Lambda adapter for executing the use case for creating a new URL hash."""
+
 from typing import Any
 import boto3  
 import os
@@ -31,6 +33,16 @@ usecase = CreateUrlHashUseCase(
 @app.post("/u")
 @tracer.capture_method
 def handle() -> dict:
+    """Wraps the use case for creating a new URL hash.
+
+    Note that we directly extract the JSON payload from the body and perform our operations. 
+    API gateway has already performed input validation so we guarenteed to have the required
+    fields when this is executed.
+
+    Returns:
+        the payload for URL redirection, including the expiration time.
+    """
+    
     payload: dict[str, Any] = app.current_event.json_body  # deserialize json str to dict
 
     response = usecase.create(
@@ -49,6 +61,19 @@ def handle() -> dict:
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
 @tracer.capture_lambda_handler
 def handle(event: dict, context: LambdaContext) -> dict:
+    """AWS Lambda entry point function.
+
+    Note that we relay on AWS Lambda Powertools for Python to process our 
+    response and set fields when needed.
+
+    Arguments:
+        event: the APIGateway event payload 
+        context: Lambda context (e.g., environment variables)
+
+    Returns:
+        Response suitable for being processed by API Gateway.
+    """
+
     logger.debug(f"Correlation ID => {logger.get_correlation_id()}")
     
     return app.resolve(event, context)
