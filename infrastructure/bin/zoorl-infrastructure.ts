93755daf579ca6a3324d4f4f6a8c4c66e0934d2d
Mario Scalas <mario.scalas@gmail.com>
2022-10-31 21:58:57 +0000

    Code formatting step.

--- infrastructure/bin/zoorl-infrastructure.ts
+++ infrastructure/bin/zoorl-infrastructure.ts
@@ -8,7 +8,7 @@ import { ZoorlPipelineStack } from "../lib/cicd/pipeline-stack";
 
 const app = new cdk.App();
 
-// This is an application stack for personal development - it is separated from 
+// This is an application stack for personal development - it is separated from
 // the other staged stacks that will be handled by the pipeline.
 new ZoorlApplicationStack(app, "ZoorlPersonalStack", {
   stage: "personal",
@@ -21,7 +21,7 @@ const pipelineStack = new ZoorlPipelineStack(app, "ZoorlPipelineStack");
 // This is from:
 // * the AWS Activate Workshop (https://catalog.us-east-1.prod.workshops.aws/workshops/13304db2-f715-48bf-ada0-92e5c4eea945/en-US/040-cicd/20-add-pipeline)
 // * Adapted from https://stackoverflow.com/a/72743464
-const permissionBoundaryArn = cdk.Fn.importValue('CICDPipelinePermissionsBoundaryArn');
+const permissionBoundaryArn = cdk.Fn.importValue("CICDPipelinePermissionsBoundaryArn");
 
-const boundary = iam.ManagedPolicy.fromManagedPolicyArn(pipelineStack, 'Boundary', permissionBoundaryArn);
+const boundary = iam.ManagedPolicy.fromManagedPolicyArn(pipelineStack, "Boundary", permissionBoundaryArn);
 iam.PermissionsBoundary.of(pipelineStack).apply(boundary);
